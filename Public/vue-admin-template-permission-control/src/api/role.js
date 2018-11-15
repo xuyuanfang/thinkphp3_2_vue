4463ff15c988296070a54beb6943b25b4cdbe553
yh <yuhai@myhexin.com>
2018-11-15 14:42:22 +0800

    默认的

--- Public/vue-admin-template-permission-control/src/api/role.js
+++ Public/vue-admin-template-permission-control/src/api/role.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 
-export function roleList(param) {
+export function roleList(params) {
     return request({
         url: 'http://localhost/thinkphp3_2_vue/index.php/Home/Role/roleList',
         method: 'get',
